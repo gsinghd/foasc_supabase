b27675481061f8d75c6817b765fe799034f6310f
gsinghd <gdhal005@ucr.edu>
2024-09-07 13:23:39 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Additional Next.js configuration options can be added here
+};
+
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  register: true, // Registers the service worker
+  skipWaiting: true, // Makes the new service worker activate as soon as it's installed
+})(nextConfig);
